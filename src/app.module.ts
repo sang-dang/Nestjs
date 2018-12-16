323e5e482df4c30e27bd1ed9bdd53eaaf7c78587
Sang Dang <sang.a5vy2@gmail.com>
2018-12-17 00:25:19 +0700

    fix path absolute when inport into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CaculateModule } from 'calculate/calculate.module';
+import { CaculateModule } from './calculate/calculate.module';
 import { CalculationModule } from './calculation/calculation.module';
-import { LoginModule } from 'login/login.module';
+import { LoginModule } from './login/login.module';
 
 @Module({
   imports: [CaculateModule, CalculationModule, LoginModule],
