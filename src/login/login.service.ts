22bfb35cd1066a2f00e526a3c169c7f190d53f00
Sang Dang <sang.a5vy2@gmail.com>
2019-01-03 08:18:44 +0700

    change login model name

--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { Ilogin } from './login.model';
+import { LoginDto } from './login.model';
 
 export const admin_username: string = 'admin';
 export const admin_password: string = '123456';
 
 @Injectable()
 export class LoginService {
-  public getIn(user: Ilogin): string {
+  public getIn(user: LoginDto): string {
     if (user.username === admin_username && user.password === admin_password) {
       return 'login successfully';
     } else {
