3b25a38e5e409f2ad35e6072edc943a9882e9169
Sang Dang <sang.a5vy2@gmail.com>
2019-01-20 22:17:13 +0700

    config for hot module replacement

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { join } from 'path';
 
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -18,5 +19,10 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3333);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
